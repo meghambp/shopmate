980ef845ea85e0238678fea13873659ae8cb0b5a
Meghakumari Patel <meghambp@gmail.com>
2024-01-10 16:34:10 -0500

    Section 7 - 2nd Commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,31 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 export const ProductList = () => {
     const [products, setProducts] = useState([]);
+    const [url, setUrl] = useState("http://localhost:8000/products");
 
-    useEffect(() => {
-        fetch("http://localhost:8000/products")
-            .then(response => response.json())
-            .then(data => setProducts(data));
-    }, [])
+    /*useEffect(() => {
+         fetch(url)
+             .then(response => response.json())
+             .then(data => setProducts(data));
+     }, [url])*/
+
+     const fetchProducts = useCallback(async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data);
+    }, [url]);
+
+    useEffect(() => {        
+        fetchProducts();
+    }, [fetchProducts]);
+
     return (
         <section>
+            <div className="filter">
+                <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
+                <button onClick={() => setUrl("http://localhost:8000/products?id>10002")}>In Stock Only</button>
+            </div>
             {products.map((product) => (
                 <div className="card" key={product.id}>
                     <p className="id">{product.id}</p>
