108d696abdd5eb2a79c0a1ae584858472e55b45f
Meghakumari Patel <meghambp@gmail.com>
2024-01-10 17:33:14 -0500

    Section 7 - Completed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,15 @@
-import { useCallback, useEffect, useState } from "react"
+import { useState } from "react"
+import { useFetch } from "../hooks/useFetch";
 
 export const ProductList = () => {
-    const [products, setProducts] = useState([]);
+    //const [products, setProducts] = useState([]);
     const [url, setUrl] = useState("http://localhost:8000/products");
-
+    const { data: products, loading, error } = useFetch(url, {content: "ABC"});
     /*useEffect(() => {
          fetch(url)
              .then(response => response.json())
              .then(data => setProducts(data));
-     }, [url])*/
+     }, [url])
 
      const fetchProducts = useCallback(async () => {
         const response = await fetch(url);
@@ -18,15 +19,17 @@ export const ProductList = () => {
 
     useEffect(() => {        
         fetchProducts();
-    }, [fetchProducts]);
+    }, [fetchProducts]);*/
 
     return (
         <section>
             <div className="filter">
                 <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
-                <button onClick={() => setUrl("http://localhost:8000/products?id>10002")}>In Stock Only</button>
+                <button onClick={() => setUrl("http://localhost:8000/products?in_stock=1")}>In Stock Only</button>
             </div>
-            {products.map((product) => (
+            {loading && <p className="loading">Product is loading...</p>}
+            {error && <p>{error}</p>}
+            {products && products.map((product) => (
                 <div className="card" key={product.id}>
                     <p className="id">{product.id}</p>
                     <p className="name">{product.name}</p>
